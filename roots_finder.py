5b83d67eac65983cc28bf9adb741463a972cf4ab
A.L <32211153+iluvjava@users.noreply.github.com>
2019-12-17 20:34:54 -0800

    stuff

--- roots_finder.py
+++ roots_finder.py
@@ -8,13 +8,20 @@ Basic idea:
     * Use Newton's method too look for the roots of the polynomials.
     * Use derivative to determine the repeated roots and them optimize it for accuracy.
 
+------------------------------------------------------------------------------------------------------------------------
+Problems:
+------------------------------------------------------------------------------------------------------------------------
+    * false evaluation of the derivative of the fixed point iterative function
+      for polynomials with extremely large degree.
+
 """
 
 from core2 import *
 from typing import Type, Dict
 MyPolynomial = Type[Polynomial]
 
-def find_root(p: MyPolynomial, x0: Number = None):
+
+def find_root(p: MyPolynomial, x0: Number=None):
     """
         Attempts to solve the polynomial at that point.
     :param p:
@@ -25,7 +32,6 @@ def find_root(p: MyPolynomial, x0: Number = None):
     x0 = random() + (-1)**(0.5)*random() if x0 is None else x0
     TOL = 1e-10
     maxitr = 1e2
-
     k = 0
     # The kth derivative
 
@@ -42,13 +48,16 @@ def find_root(p: MyPolynomial, x0: Number = None):
             x1 = g(x1)
             itr += 1
 
+        # Blocks invalid solution:
+        assert abs(p.eval_at(x1)) < 1e-4, f"Grave Error omg. x1 = {x1} "
+
+
         # Checking the fixed point function result for repeated roots.
-        dgdx = (g(x1+1e-4) - g(x1))/1e-4
+        dgdx = (g(x1 + 1e-8) - g(x1))/1e-8
         if abs(dgdx) < 1e-2:
-            return x0, k + 1
+            return x1, k + 1
         k += 1
         continue
-
     return None # Not converging.
 
 
@@ -97,49 +106,53 @@ def find_roots(p: MyPolynomial, results:Dict[Number, int] = None, precision:str 
 
 
 if __name__ == "__main__":
-    p = Polynomial([1, 2, 1])
-    print(find_root(p))
+    # p = Polynomial([1, 2, 1])
+    # print(find_root(p))
+    #
+    # p = Polynomial([1, 4, 6, 4, 1])
+    # print(find_root(p))
+    #
+    # p = Polynomial([1, 5, 10, 10, 5, 1])
+    # print(find_root(p))
+    #
+    # p = Polynomial([1, -9, 33, -63, 66, -36, 8])
+    # print(f"Let's try on a hard one: {p}")
+    # root = find_root(p)
+    # print(f"This is one of the root with multiplicity: f{root}")
+    # print("Try to factor it out: ")
+    # p = p.factor_out(root[0], poly=True, multiplicity=root[1])
+    # print(p)
+    # print("Try to solve it again: ")
+    # root = find_root(p)
+    # print(f"root = {root}")
+    #
+    #
+    # print("try to find all roots at once: ")
+    # p = Polynomial([1, -9, 33, -63, 66, -36, 8])
+    # print(f"This is the polynomial: {p}")
+    # print(find_roots(p))
+    #
+    # print("try to find all roots at once: ")
+    # p = Polynomial([1, 1, 1])
+    # print(f"This is the polynomial: {p}")
+    # print(find_roots(p))
+    #
+    # print("try to find all roots at once: ")
+    # p = Polynomial([1, 0, -2])
+    # print(f"This is the polynomial: {p}")
+    # print(find_roots(p))
+    #
+    # print("Try to find all roots with a precision parameters")
+    # print(find_roots(p, precision="High"))
+    #
+    # p = Polynomial({8:1, 0:-1})
+    # print(f"Try to find the roots for: {p}")
+    # print(find_roots(p, precision="high"))
 
-    p = Polynomial([1, 4, 6, 4, 1])
-    print(find_root(p))
+    # p = Polynomial({10: 1, 0: -1})
+    # print(f"Try to find the roots for: {p}")
+    # print(find_roots(p, precision="high"))
 
-    p = Polynomial([1, 5, 10, 10, 5, 1])
-    print(find_root(p))
-
-    p = Polynomial([1, -9, 33, -63, 66, -36, 8])
-    print(f"Let's try on a hard one: {p}")
-    root = find_root(p)
-    print(f"This is one of the root with multiplicity: f{root}")
-    print("Try to factor it out: ")
-    p = p.factor_out(root[0], poly=True, multiplicity=root[1])
-    print(p)
-    print("Try to solve it again: ")
-    root = find_root(p)
-    print(f"root = {root}")
-
-
-    print("try to find all roots at once: ")
-    p = Polynomial([1, -9, 33, -63, 66, -36, 8])
-    print(f"This is the polynomial: {p}")
-    print(find_roots(p))
-
-    print("try to find all roots at once: ")
-    p = Polynomial([1, 1, 1])
-    print(f"This is the polynomial: {p}")
-    print(find_roots(p))
-
-    print("try to find all roots at once: ")
-    p = Polynomial([1, 0, -2])
-    print(f"This is the polynomial: {p}")
-    print(find_roots(p))
-
-    print("Try to find all roots with a precision parameters")
-    print(find_roots(p, precision="High"))
-
-    p = Polynomial({8:1, 0:-1})
-    print(f"Try to find the roots for: {p}")
-    print(find_roots(p, precision="high"))
-
-    p = Polynomial({20: 1, 0: -1})
+    p = Polynomial({50: 1, 0: -1})
     print(f"Try to find the roots for: {p}")
     print(find_roots(p, precision="high"))
